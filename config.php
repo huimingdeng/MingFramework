cbe7c7a8ca4a7631f416690ad5557b4f0b5ad66e
huimingdeng <13570313864@163.com>
2019-03-04 12:05:10 +0800

    修改常量名

--- config.php
+++ config.php
@@ -9,6 +9,6 @@
 define(APP_NAME,"MingFrameWork");
 define(VERSION,1.0);
 define(DB_TYPE,"mysql");
-define(DB_CONN,"localhost");
+define(DB_HOST,"localhost");
 define(DB_USER,"root");
 define(DB_PASSWD,"");

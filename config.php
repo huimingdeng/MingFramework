9ec7dadbc961003ab5a2c088403b1b31bef6d2c0
huimingdeng <13570313864@163.com>
2019-03-06 15:25:03 +0800

    修复忘记测试的常量错误

--- config.php
+++ config.php
@@ -6,11 +6,11 @@
  * Time: 11:12
  */
 
-define(APP_NAME,"MingFrameWork");
-define(VERSION,1.0);
-define(DB_TYPE,"mysql");
-define(DB_HOST,"localhost");
-define(DB_USER,"homestead");
-define(DB_PASSWD,"secret");
-define(DB_PORT,3306);
-define(DB_DATABASE,"homestead");
+define("APP_NAME","MingFrameWork");
+define("VERSION","1.0");
+define("DB_TYPE","mysql");
+define("DB_HOST","localhost");
+define("DB_USER","homestead");
+define("DB_PASSWD","secret");
+define("DB_PORT",3306);
+define("DB_DATABASE","homestead");

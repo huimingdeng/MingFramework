f7a66395d051d7baf206c2a4538ab749ae5d89c9
huimingdeng <13570313864@163.com>
2019-06-30 10:38:26 +0800

    Macaw 路由安装与测试

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<?php 
+<?php
 /**
  * create by sublime text 3
  * Author : huimingdeng
  * create at : 2/21/2019 16:55
  */
 // 使用第三方类库，自己编写的自动加载函数可能会存在问题，因此使用
-include "vendor\autoload.php";
-\core\Bootstrap::run();
+require_once __DIR__ . "/vendor/autoload.php";
+// \core\Bootstrap::run();
 
+use \NoahBuscher\Macaw\Macaw;
+Macaw::get('/', function () {
+	echo 'Welcome to Bee!';
+});
+Macaw::dispatch();

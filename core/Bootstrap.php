7002c06870a5f3450616f5aaeade1290d9267b19
huimingdeng <13570313864@163.com>
2019-06-30 10:42:57 +0800

    旧版本自造路由轮子舍弃，使用第三方路由

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -6,31 +6,31 @@
  * Time: 20:23
  */
 namespace core;
-class Bootstrap{
-    /**
-     * 执行
-     */
-    public static function run(){
-        session_start();
+class Bootstrap {
+	/**
+	 * 执行
+	 */
+	public static function run() {
+		session_start();
 //        echo "my is bootstrap::run";
-        self::parseUrl();
-    }
+		self::parseUrl();
+	}
 
-    /**
-     * 解析 url
-     */
-    public static function parseUrl(){
-        if (isset($_GET['s'])){
-            // 分析 s 变量，生成控制器
-            $s = explode('/', $_GET['s']);
-            $class = '\web\controller\\'.ucfirst($s[0]).'Controller';
-            $action = $s[1];
+	/**
+	 * 解析 url 路由解析实现
+	 */
+	public static function parseUrl() {
+		if (isset($_GET['s'])) {
+			// 分析 s 变量，生成控制器
+			$s = explode('/', $_GET['s']);
+			$class = '\web\controller\\' . ucfirst($s[0]) . 'Controller';
+			$action = $s[1];
 //            dd($s);
-        }else{
-            $class = "\web\controller\IndexController";
-            $action = "show";
-        }
-        echo ( new $class ) -> $action();
-        // dd($_SERVER);
-    }
+		} else {
+			$class = "\web\controller\IndexController";
+			$action = "show";
+		}
+		echo (new $class)->$action();
+		// dd($_SERVER);
+	}
 }
